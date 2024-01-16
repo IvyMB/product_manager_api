97de4cfe4aa44791adf3372a2f54973e59155c3c
Iveny Milena B S <vyhofc@gmail.com>
2024-01-16 18:06:16 -0300

    Update product_schema.py
    fix: category_id json response

--- app/schemas/product_schema.py
+++ app/schemas/product_schema.py
@@ -5,6 +5,6 @@ class ProductSchema(Schema):
     id = fields.Str(dump_only=True)
     title = fields.Str(required=True, validate=validate.Length(max=50))
     description = fields.Str(required=True, validate=validate.Length(max=500))
-    category = fields.Str(required=True)
+    category = fields.Str(required=True, attribute='category.id')
     price = fields.Str(required=True)
     owner_id = fields.Str(required=True)
