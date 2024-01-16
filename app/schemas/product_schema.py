ddc9c27656207c6166d8c38ffa1dbfca2a178018
Iveny Milena B S <vyhofc@gmail.com>
2024-01-16 18:52:51 -0300

    Update product_schema.py

--- app/schemas/product_schema.py
+++ app/schemas/product_schema.py
@@ -5,6 +5,6 @@ class ProductSchema(Schema):
     id = fields.Str(dump_only=True)
     title = fields.Str(required=True, validate=validate.Length(max=50))
     description = fields.Str(required=True, validate=validate.Length(max=500))
-    category = fields.Str(required=True, attribute='category.id')
+    category = fields.Str(required=True)
     price = fields.Str(required=True)
     owner_id = fields.Str(required=True)
