79db30465990dc6f1a53c096def32fd32d39a675
Iveny Milena B S <vyhofc@gmail.com>
2024-01-16 18:28:44 -0300

    Update category_view.py
    refactor category_view

--- app/views/category_view.py
+++ app/views/category_view.py
@@ -3,6 +3,7 @@ from flask.views import MethodView
 from ..services import CategoryService
 from ..schemas import CategorySchema
 from ..dtos import CategoryDTO
+from app.exceptions.category_exceptions import CategoryNotFoundError, CategoryAlreadyExistsError
 
 
 class CategoryView(MethodView):
@@ -13,14 +14,15 @@ class CategoryView(MethodView):
 
     def get(self, category_id=None):
         if category_id:
-            category = self.category_service.get_by_id(category_id)
-            if not category:
-                return jsonify({'message': 'Category already exists'}), 404
+            try:
+                category = self.category_service.get_by_id(category_id)
+            except CategoryNotFoundError:
+                return jsonify({'errors': 'Category not found'}), 404
 
             result = self.schema.dump(category)
             return jsonify(result), 200
 
-        all_categories = self.category_service.get_all().decode('utf8')
+        all_categories = self.category_service.get_all()
         result = self.many_schema.dump(all_categories)
         return jsonify(result), 200
 
@@ -31,11 +33,12 @@ class CategoryView(MethodView):
             return jsonify({'errors': errors}), 400
 
         category_dto = CategoryDTO(**category_data)
-        new_category = self.category_service.create(category_dto)
-
-        if not new_category:
+        try:
+            new_category = self.category_service.create(category_dto)
+        except CategoryAlreadyExistsError:
             return jsonify({'message': 'Category already exists'}), 404
-        result = self.schema.dump(new_category).decode('utf8')
+
+        result = self.schema.dump(new_category)
         return jsonify(result), 201
 
     def put(self, category_id=None):
@@ -45,16 +48,17 @@ class CategoryView(MethodView):
             return jsonify({'errors': errors}), 400
 
         category_dto = CategoryDTO(**category_data)
-        updated_category = self.category_service.update(category_id, category_dto)
-        if not updated_category:
-            return jsonify({'errors': 'Product not found'}), 404
-
-        result = self.schema.dump(updated_category).decode('utf8')
+        try:
+            updated_category = self.category_service.update(category_id, category_dto)
+        except CategoryNotFoundError:
+            return jsonify({'errors': 'Category not found'}), 404
+        
+        result = self.schema.dump(updated_category)
         return jsonify(result), 200
 
     def delete(self, category_id=None):
         if category_id is None:
             return jsonify({'message': 'Product not found'}), 400
-
+        
         self.category_service.category_delete(category_id)
         return jsonify({'message': 'Product deleted successfully'}), 200
