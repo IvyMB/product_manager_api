2d8ad06107a5dd7ef226d995580ff48699c7fba3
Iveny Milena B S <vyhofc@gmail.com>
2024-01-20 12:17:47 -0300

    Update product_blueprint.py
    typing the id

--- app/blueprints/product_blueprint.py
+++ app/blueprints/product_blueprint.py
@@ -5,4 +5,4 @@ product_blueprint = Blueprint('product', __name__)
 product_view = ProductView.as_view('product_view')
 
 product_blueprint.add_url_rule('/api/products', view_func=product_view, methods=['GET', 'POST'])
-product_blueprint.add_url_rule('/api/products/<product_id>', view_func=product_view, methods=['GET', 'PUT', 'DELETE'])
+product_blueprint.add_url_rule('/api/products/<string:product_id>', view_func=product_view, methods=['GET', 'PUT', 'DELETE'])
