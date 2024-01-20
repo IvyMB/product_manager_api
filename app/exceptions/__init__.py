97c6bb5f4f77e024b94cc4ab738613f722c8a32a
Iveny Milena B S <vyhofc@gmail.com>
2024-01-20 16:04:07 -0300

    Update __init__.py

--- app/exceptions/__init__.py
+++ app/exceptions/__init__.py
@@ -1,2 +1,3 @@
 from .category_exceptions import CategoryNotFoundError
 from .product_exceptions import ProductAlreadyExistsError, ProductNotFoundError
+from .user_exceptions import UserAlreadyExistsError, UserNotFoundError, WrongCredentialsError
