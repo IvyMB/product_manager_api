ebb6f2d8d8dc52f1d2a63b08713d71a3e652605a
Iveny Milena B S <vyhofc@gmail.com>
2024-03-13 19:18:41 -0300

    Update login_view.py

--- app/views/login_view.py
+++ app/views/login_view.py
@@ -1,6 +1,5 @@
 from flask import jsonify, request
 from flask.views import MethodView
-from ..services import UserService
 from ..usecases import UserUseCase
 from ..schemas import LoginSchema
 from ..dtos import LoginDTO
@@ -9,7 +8,6 @@ from ..exceptions import WrongCredentialsError, UserNotFoundError
 
 class LoginView(MethodView):
     def __init__(self):
-        self.user_service = UserService()
         self.user_usecase = UserUseCase(self.user_service)
         self.login_schema = LoginSchema()
 
